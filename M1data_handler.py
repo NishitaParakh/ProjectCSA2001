a83cdd9a0ebf629c88c2d5a0a5bfab7166251d95
NishitaParakh <parakhnishita14@gmail.com>
2026-03-27 21:39:15 +0530

    M1 updated

--- M1data_handler.py
+++ M1data_handler.py
@@ -6,15 +6,15 @@ file = os.path.join(os.path.dirname(__file__), "expenses.csv")
 
 def init_file():
     if not os.path.exists(file):
-        df = pd.DataFrame(columns = ["Amount", "category", "date"])
+        df = pd.DataFrame(columns = ["Amount", "Category", "Date"])
         df.to_csv(file, index = False)
 
 def add_expense(amount, category):
     df = pd.read_csv(file)
-    date = datetime.now().strftime("%Y:%m:%d")
+    date = datetime.now().strftime("%Y-%m-%d")
     new_data = {
         "Amount" : amount, 
         "Category" : category, 
         "Date" : date}
-    df = pd.concat([df, pd.DataFrame([new_data])], ignore_index = False)
-    df = df.to_csv(file)
+    df = pd.concat([df, pd.DataFrame([new_data])], ignore_index = True)
+    df.to_csv(file)
